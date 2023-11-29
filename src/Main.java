a159ccf81528641e9dfdd1ba5adc312b8318ddc8
João Vianna <joovictorvianna@hotmail.com>
2023-11-29 14:12:58 -0300

    terminando configs

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        String saudacao = "Olá, meu nome é ";
+        String nome = "Alice ";
+        String continuacao = "e minha idade é ";
+        int idade = 17;
+        System.out.println(saudacao + nome + continuacao + idade);
+
+    }
+}
